e7c32e8275edfd1c02a28091370a0ab64ec93a63
User <lcsgoes210@gmail.com>
2023-03-11 11:50:45 -0300

    commit

--- __main__.py
+++ __main__.py
@@ -7,9 +7,9 @@ load_dotenv()
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 def LojaAberta():
-    tickets = 1067933165605372036
+    ticket = 1067933165605372036
     guild = bot.get_guild(1067626063410253874)
-    return bot.get_channel(tickets).permissions_for(guild.default_role).view_channel
+    return bot.get_channel(ticket).permissions_for(guild.default_role).view_channel
 
 @bot.event
 async def on_ready():
@@ -26,26 +26,22 @@ async def on_message(message):
         return
     
     msg = message.content.lower()
-    if len(re.findall("loja|aberta|fechada", msg)) >= 2 and msg.endswith("?"):
-        tickets = 1067933165605372036
-        horarios = 1070775740653654027
+    if re.findall(".*loja.*aberta|fechada.*", msg) and msg.endswith("?"):
+        ticket = 1067933165605372036
         lojaaberta = LojaAberta()
         response = ""
         if lojaaberta != False:
-            response = f"A loja está aberta! Vá em <#{tickets}> para fazer um pedido!"
+            response = f"A loja está aberta! Vá em <#{ticket}> para fazer um pedido!"
         else:
-            response = f"A loja infelizmente está fechada. Verifique os <#{horarios}> caso queira abrir um mais tarde!"
+            response = f"A loja infelizmente está fechada."
         await message.reply(response)
-    elif re.findall(".*como.*compra", msg) or re.findall(".*como.*abre.*ticket.*", msg):
+    elif re.findall(".*como.*compra.*", msg) or re.findall(".*como.*abre.*ticket.*", msg):
         comocomprar = 1067904352292970606
-        lojaaberta = LojaAberta()
-        response = ""
-        if lojaaberta != False:
-            response = f"Verifique o <#{comocomprar}>."
-        else:
-            response = f"Atualmente a loja está fechada, mas verifique o <#{comocomprar}>."
-        await message.reply(response)
-    elif (re.findall("vendo|vendendo", msg) or (re.findall("troco|lf|procuro|procurando", msg) and re.findall("pix|p1x|robux", msg))) and message.channel.id == 1068366008517140500:
+        await message.reply(f"Verifique o <#{comocomprar}>.")
+    elif re.findall(".*que.*hora.*abre|fecha|atende.*", msg):
+        ticket = 1067933165605372036
+        await message.reply(f"Nós normalmente abrimos o <#{ticket}> das 17:00 as 22:30.")
+    elif (re.findall("vendo|vendendo", msg) or (re.findall("troco|lf|procuro|procurando", msg) and re.findall(" pix | p1x | robux ", msg))) and message.channel.id == 1068366008517140500:
         roles = [1069360991055388683, 1079960179774341170, 1067999699367374878, 1069163179638259733]
         if roles.count(message.author.top_role.id):
             embed = discord.Embed(title="Possível venda detectada.", description=f"{message.content} ([Vá para a mensagem]({message.jump_url}))", color=0x990000)
@@ -54,7 +50,7 @@ async def on_message(message):
             await bot.get_channel(1071578248930131998).send("<@&1071267815316803694> <@&1068396717705273384>", embed=embed)
     elif msg == "<@843250104437571614> x <@1052328074399719585>" or msg == "<@1052328074399719585> x <@843250104437571614>":
         await message.reply("dois gay q eu amo mt :heart_eyes:")
-        # oi madu :)
+        # https://docs.google.com/document/d/1qy_wclat0mKZen2NuGWqw6EIMgOJkmsWJrHtbDMxv5k/edit?usp=sharing
 
 @bot.tree.command(name="server", description="Mostra o link do servidor privado.")
 async def server(interaction: discord.Interaction):
@@ -65,5 +61,13 @@ async def pix(interaction: discord.Interaction):
     embed = discord.Embed(title="QR Code - PIX", description="Ou use a chave aleatória inserida no conteúdo da mensagem.", color=0xE52D2D)
     embed.set_image(url="https://fsmm2.github.io/images/QR.png")
     await interaction.response.send_message("841a1001-12ec-46ae-8358-327595617102", embed=embed)
+
+@bot.tree.command(name="cliente", description="Dá o cargo de cliente para alguém.")
+async def pix(interaction: discord.Interaction):
+    roles = [1071267815316803694]
+    if roles.count(interaction.user.top_role.id):
+        await interaction.response.send_message("ainda to fazendo calmai kkj", ephemeral=True)
+    if roles.count(interaction.user.top_role.id):
+        await interaction.response.send_message(":x: | Você não tem permissão de fazer isso!", ephemeral=True)
 
 bot.run(os.getenv("TOKEN"))
