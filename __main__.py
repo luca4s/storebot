eb6556fdc3e7274261a1ea0c45c631420fe159f8
User <lcsgoes210@gmail.com>
2023-03-12 10:53:09 -0300

    commit

--- __main__.py
+++ __main__.py
@@ -40,17 +40,17 @@ async def on_message(message):
     elif re.findall(".*como.*compra.*", msg) or re.findall(".*como.*abre.*ticket.*", msg):
         comocomprar = 1067904352292970606
         await message.reply(f"Verifique o <#{comocomprar}>.")
-    elif re.findall(".*que.*hora.*abre.*", msg):
+    elif re.findall(".*que.*hora.*abre.*", msg) or re.findall(".*quando.*abre.*", msg) or re.findall(".*loja.*abre.*", msg):
         ticket = 1067933165605372036
         await message.reply(f"Nós normalmente abrimos o <#{ticket}> das 17:00 as 22:30.")
-    elif (re.findall("vendo|vendendo", msg) or (re.findall("troco|lf|procuro|procurando", msg) and re.findall(" pix | p1x | robux ", msg))) and message.channel.id == 1068366008517140500:
+    elif (re.findall("vendo|vendendo", msg) or (re.findall("troco|lf|procuro|procurando", msg) and re.findall(" pix| p1x | robux ", msg))) and message.channel.id == 1068366008517140500:
         roles = [1069360991055388683, 1079960179774341170, 1067999699367374878, 1069163179638259733]
         if roles.count(message.author.top_role.id):
             embed = discord.Embed(title="Possível venda detectada.", description=f"{message.content} ([Vá para a mensagem]({message.jump_url}))", color=0x990000)
             embed.set_author(name=message.author, icon_url=message.author.avatar.url)
             embed.set_footer(text=f"ID Mensagem: {message.id}\nID Remetente: {message.author.id}")
             await bot.get_channel(1071578248930131998).send("<@&1071267815316803694> <@&1068396717705273384>", embed=embed)
-    elif msg == "<@843250104437571614> x <@1052328074399719585>" or msg == "<@1052328074399719585> x <@843250104437571614>":
+    elif msg == "<@1052666107451949137> x <@1052328074399719585>" or msg == "<@1052328074399719585> x <@1052666107451949137>":
         await message.reply("dois gay q eu amo mt :heart_eyes:")
         # https://docs.google.com/document/d/1qy_wclat0mKZen2NuGWqw6EIMgOJkmsWJrHtbDMxv5k/edit?usp=sharing
 
@@ -69,11 +69,16 @@ async def pix(interaction: discord.Interaction):
 async def cliente(interaction: discord.Interaction, user: discord.Member):
     roles = [1082368752512933978, 1071267815316803694, 1068721364892139541, 1068396717705273384, 1075148178543890582, 1067999262266376272, 1069723376756719697]
     if roles.count(interaction.user.top_role.id):
-        await interaction.response.send_message("madu se vc estiver lendo isso, bota o cargo do bot acima do @Clientes pra esse comando funcionar (ja programei, mas n funciona pq o bot ta abaixo de clientes)", ephemeral=True)
-        # guild = bot.get_guild(1067626063410253874)
-        # cliente = guild.get_role(1067999699367374878) 
-        # await user.add_roles(cliente)
-        # await interaction.response.send_message("Pronto!", ephemeral=True)
+        if user:
+            guild = bot.get_guild(1067626063410253874)
+            cliente = guild.get_role(1067999699367374878)
+            if guild.get_member(bot.user.id).top_role.position > cliente.position:
+                await user.add_roles(cliente)
+                await interaction.response.send_message("Pronto!", ephemeral=True)
+            else:
+                await interaction.response.send_message(":warning: | O bot não tem a permissão de dar este cargo!", ephemeral=True)
+        else:
+            await interaction.response.send_message(":warning: | Este usuário não existe! Use apenas menções, tags e IDs.", ephemeral=True)
     else:
         await interaction.response.send_message(":x: | Você não tem permissão de fazer isso!", ephemeral=True)
 
